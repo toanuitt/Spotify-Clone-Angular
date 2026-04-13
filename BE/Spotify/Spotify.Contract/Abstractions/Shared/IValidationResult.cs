761006cce994f16898950169fa0312087e315828
Work GitHub <toanpd@gfigroup.io>
2026-04-13 15:23:21 +0700

    Feat: add domain identity

--- BE/Spotify/Spotify.Contract/Abstractions/Shared/IValidationResult.cs
+++ BE/Spotify/Spotify.Contract/Abstractions/Shared/IValidationResult.cs
@@ -2,7 +2,7 @@
 
 public interface IValidationResult
 {
-    public static readonly Error ValidationError = new(
+    static readonly Error ValidationError = new(
         "ValidationError",
         "A validation problem occurred.");
     Error[] Errors { get; }
---
+++ BE/Spotify/Spotify.Domain/Entities/Identity/Function.cs
@@ -0,0 +1,15 @@
+﻿namespace Spotify.Domain.Entities.Identity;
+
+public class Function
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public string ParentId { get; set; }
+    public int? SortOrder { get; set; }
+    public string CssClass { get; set; }
+    public bool? IsActive { get; set; }
+
+    public virtual ICollection<Permission> Permissions { get; set; }
+    public virtual ICollection<ActionInFunction> ActionInFunctions { get; set; }
+}
